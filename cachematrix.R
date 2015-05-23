530cc723969af8e22ea43da53ee291a487e450d6
RVGUNAM01 <rene.v.act@gmail.com>
2015-05-23 12:39:58 -0500

    Final
    Final

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+########### R Programming Assignment 2 ###########
+################### April 2015 ###################
 
-## Write a short comment describing this function
+## makeCacheMatrix: This function creates a special "matrix" object 
+## that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()) { 
+        m <- NULL 
+        set <- function(y) { 
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x 
+        setinv <- function(invr) m <<- invr 
+        getinv <- function() m 
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv) 
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+        m <- x$getinv()
+        if(!is.null(m)) { 
+                message("...getting cached data...")
+                return(m) 
+        }
+        data <- x$get() 
+        m <- solve(data, ...) 
+        x$setinv(m) 
+        return(m)
 }
+
+######################### EXAMPLE #########################
+
+w <- c(1,2,3,4) 
+
+a <- makeCacheMatrix(matrix(w,2)) 
+
+cacheSolve(a)
+
+
+cacheSolve(a)
+
+## Running the example code gives us:
+#     [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+# >
+# > cacheSolve(a)
+#      [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
